7524c7dc631346f4f0aa7294d5f1e854347f3f6e
Derik Whittaker <derik@graudo.com>
2013-09-24 05:58:59 -0400

    WIPfor Typescript demo

---
+++ TypeScript/D.01.TypeDefinitionFiles/Clean/D.01.TypeDefinition/ViewModels/MainViewModel.js
@@ -0,0 +1,27 @@
+var TypeDefinitions;
+(function (TypeDefinitions) {
+    var MainViewModel = (function () {
+        function MainViewModel() {
+            this.Items = ko.observableArray([]);
+            this.Items.push(new ToDoItem("Item 1", "Description 1"));
+            this.Items.push(new ToDoItem("Item 2", "Description 2"));
+            this.Items.push(new ToDoItem("Item 3", "Description 3"));
+            this.Items.push(new ToDoItem("Item 4", "Description 4"));
+            this.Items.push(new ToDoItem("Item 5", "Description 5"));
+            this.Items.push(new ToDoItem("Item 6", "Description 6"));
+        }
+        return MainViewModel;
+    })();
+    TypeDefinitions.MainViewModel = MainViewModel;
+
+    var ToDoItem = (function () {
+        function ToDoItem(name, description) {
+            this.Name = ko.observable("");
+            this.Description = ko.observable("");
+            this.Name(name);
+            this.Description(description);
+        }
+        return ToDoItem;
+    })();
+    TypeDefinitions.ToDoItem = ToDoItem;
+})(TypeDefinitions || (TypeDefinitions = {}));
---
+++ TypeScript/D.02.TypeChecking/Clean/D.01.TypeDefinition/ViewModels/MainViewModel.ts
@@ -0,0 +1,26 @@
+
+module TypeDefinitions {
+    export class MainViewModel {
+
+        public Items: KnockoutObservableArray<ToDoItem> = ko.observableArray([]);
+
+        constructor() {
+            this.Items.push(new ToDoItem("Item 1", "Description 1"));
+            this.Items.push(new ToDoItem("Item 2", "Description 2"));
+            this.Items.push(new ToDoItem("Item 3", "Description 3"));
+            this.Items.push(new ToDoItem("Item 4", "Description 4"));
+            this.Items.push(new ToDoItem("Item 5", "Description 5"));
+            this.Items.push(new ToDoItem("Item 6", "Description 6"));
+        }
+    }
+
+    export class ToDoItem{
+        public Name: KnockoutObservable<string> = ko.observable("");
+        public Description: KnockoutObservable<string> = ko.observable("");
+
+        constructor(name: string, description: string) {
+            this.Name(name);
+            this.Description(description);
+        }
+    }
+}
